5d630b79837aab925d480e4adf747ff3762a9e14
Felipe Movio <felipelsmovio@gmail.com>
2026-08-06 11:22:17 -0300

    inicial

---
+++ FilmesApi/Controllers/FilmesController.cs
@@ -0,0 +1,20 @@
+﻿using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using static System.Net.WebRequestMethods;
+
+namespace FilmesApi.Controllers;
+
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : Controller
+{
+
+    private static List<Filme> filmes = new List<Filme>();
+
+    [HttpPost]
+    public void AdicionarFilme([FromBody] Filme filme)
+    {
+        filmes.Add(filme);
+    }
+}
